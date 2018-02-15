f1c53ced049e8d569bb1255baf44767ba16f3be2
Taldarion <fasch00@icloud.com>
2018-02-15 16:35:57 +0100

    Update RenderManager.java
    Erstellt nun ein grundlegendes Fenster, auf welchem gezeichnet werden kann.

--- Grafik/RenderManager.java
+++ Grafik/RenderManager.java
@@ -1,32 +1,36 @@
 package Grafik;
 
+import java.awt.BorderLayout;
 import java.awt.Graphics;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 class RenderManager{				// Hauptklasse in der Grafik, managed wann was gezeichnet wird
-	private State state;					// In welchem State befindet sich das Programm? z.B Hauptmenü, Charakterauswahl, etc.
-	private JFrame f;						// Fenster auf dem alles dargestellt wird
-	private DrawingPanel p;					// eigene Panelklasse in der alles gezeichnet wird
+	private State state;					// In welchem State befindet sich das Programm? z.B HauptmenÃ¼, Charakterauswahl, etc.
+	private static JFrame f;				// Fenster auf dem alles dargestellt wird
+	private static DrawingPanel p;				// eigene Panelklasse in der alles gezeichnet wird
 	
 	
 	public static void main(String[] args) {
-		
+		f = new JFrame("Project-X");
+		f.setSize(1024, 760);
+		f.setVisible(true);
+		f.setLayout(new BorderLayout());
+		p = new DrawingPanel();
+		f.add(p, BorderLayout.CENTER);
 	}
 	
-	
-	
-	
-	
-	
-	
-	@SuppressWarnings("serial")
-	private class DrawingPanel extends JPanel{
-		@Override
-		public void paintComponents(Graphics g) {
-			
-		}
-	}
 }
 
+@SuppressWarnings("serial") 
+class DrawingPanel extends JPanel{
+	
+	public DrawingPanel() {
+		super();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.fillRect(40, 50, 30, 90);
+	}
+}
