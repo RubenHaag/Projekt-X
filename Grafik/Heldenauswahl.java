7a289cd87feecf9cf93d5176815203e8080f8819
Unknown <rubenhaag1@gmail.com>
2018-02-15 14:18:38 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Grafik/Heldenauswahl.java
@@ -0,0 +1,21 @@
+package Grafik;
+
+import java.awt.Graphics;
+
+class Heldenauswahl extends Charakterauswahl{
+
+
+	@Override
+	public void drawCharakterauswahl(Graphics g) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	@Override
+	public String getCharakter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
---
+++ Grafik/Image.java
@@ -0,0 +1,13 @@
+package Grafik;
+
+import java.awt.Graphics;
+
+class Image{		// statisches Bild ohne Animation
+	
+	// zeichnet das übergebene Bild an die übergebene Position
+	public static void drawImage(Graphics g, int x, int y, int width, int height, String dateiname){
+		
+	}
+
+}
+
