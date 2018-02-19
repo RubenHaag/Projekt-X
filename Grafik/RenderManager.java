a4e315e0e3e3168d5a44e8def3a49617627e649f
Taldarion <fasch00@icloud.com>
2018-02-19 20:37:59 +0100

    Update RenderManager.java
    Kleine Änderungen und kleiner Button-Test.

--- Grafik/RenderManager.java
+++ Grafik/RenderManager.java
@@ -2,13 +2,48 @@ package Grafik;
 
 import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 class RenderManager{				// Hauptklasse in der Grafik, managed wann was gezeichnet wird
 	private State state;					// In welchem State befindet sich das Programm? z.B Hauptmenü, Charakterauswahl, etc.
-	private static JFrame f;				// Fenster auf dem alles dargestellt wird
-	private static DrawingPanel p;				// eigene Panelklasse in der alles gezeichnet wird
+	private static JFrame f;						// Fenster auf dem alles dargestellt wird
+	private static MenueButton b;
+	private static MouseListener ml = new MouseListener() {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			System.out.println("Hi, ich bin ein Knopf.");
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
 	
 	
 	public static void main(String[] args) {
@@ -16,21 +51,16 @@ class RenderManager{				// Hauptklasse in der Grafik, managed wann was gezeichne
 		f.setSize(1024, 760);
 		f.setVisible(true);
 		f.setLayout(new BorderLayout());
-		p = new DrawingPanel();
-		f.add(p, BorderLayout.CENTER);
+		b = new MenueButton("Test", ml, 50, 60, 70, 80);
+		f.add(b.getPanel(), BorderLayout.CENTER);
 	}
 	
-}
-
-@SuppressWarnings("serial") 
-class DrawingPanel extends JPanel{
-	
-	public DrawingPanel() {
-		super();
-	}
-	
-	@Override
-	public void paint(Graphics g) {
-		g.fillRect(40, 50, 30, 90);
+	public void render() {
+		switch(state) {
+		case HAUPTMENUE: 
+			break;
+		default:
+			break;
+		}
 	}
 }
