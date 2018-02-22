52f13cc1ac428bf9de28c82fab5c52c95a54c514
OskarHowe <35808038+OskarHowe@users.noreply.github.com>
2018-02-22 16:54:18 +0100

    Client Version 0.2.1
    ShakeHands(), genannt hell0(), ist implementiert (der Client bekommt vom Server einen Port zugewisen, an den der Client ab jetzt die Packets versenden soll)
    Ich habe ohne java.NIO gearbeitet, da es sich beim gegenseitigen austausch der IPaddressen als komplzierter herausgestellt hat. Beim neu verwendetetn DatagramPacket kann man direkt auslesen, von welcher IP und welchem Port gesendet wurde.

--- Client-Server-Kommunikation/Client-Server (Version 0.1)/TestServer (Dateien nicht aus diesen Ordner entfernen)/Client.java
+++ Client-Server-Kommunikation/Client-Server (Version 0.1)/TestServer (Dateien nicht aus diesen Ordner entfernen)/Client.java
@@ -2,106 +2,132 @@ import java.io.IOException;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
-import java.net.UnknownHostException;
 
 /*
  * @author Oskar Moritz
- * @version 0.1
+ * @version 0.2.1
  */
 
 public class Client extends Thread{
-  
-  private int port;
-  private DatagramChannel c1;
-  private SocketAddress address;
-  private ByteBuffer bb;
-  private Spieler sself; //bekommen wir von Lukas im z.b. Konstruktor
-  String adr;
-  private InetAddress myIP; 
-  
-  
-  public Client(String adr, int port, Spieler s) {
-    this.sself = s;
-    this.port = port;
-    this.adr= adr;
-    bb = ByteBuffer.allocate(1024); 
-    SocketAddress socketAddress = new InetSocketAddress(adr, this.port);  //socketAddress
-    this.address = socketAddress;
-    connect();
-  }
-  
-  public Client(int port, Spieler s)throws IOException {
-    this.sself = s;
-    this.port = port;
-    myIP = InetAddress.getLocalHost();
-    bb = ByteBuffer.allocate(1024); 
-    SocketAddress socketAddress = new InetSocketAddress(myIP, this.port);  //socketAddress
-    this.address = socketAddress;
-    connect();
-  } 
-  
-  public void run() {
-    while(true) {
-      System.out.println("Going to send soon");
-      send();
-      //read();
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }    
+	
+	private int port;
+	private DatagramChannel c1;
+	private SocketAddress address;
+	private ByteBuffer bb;
+	private Spieler sself; //bekommen wir von Lukas im z.b. Konstruktor
+	private InetAddress ip;
 
-    }
-  }
-  /**
-     *  Metode zum senden, der Daten.
-     */
-  private void send(){
-    bb.clear();
-    bb.put(sself.getname().getBytes());
-    //bb.putDouble(sself.getZahl().getBytes());
-    if(bb.position() != 0) {
-      System.out.println("der sendbuffer enthält daten");
-    }
-    
-    bb.flip();
-    try {
-      c1.send(bb, address);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    bb.clear(); 
-  }
-  
-  private void read() {
-    try {
-      c1.receive(bb);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }   
-    System.out.println("kurz vor if abfrage");
-    if(bb.position() != 0) {
-      bb.flip();
-      System.out.println("first Int: "+bb.get());
-      System.out.println("first double: "+bb.getDouble());
-      //sself.setUUID(bb.getInt());
-      //sself.setZahl(bb.getDouble());
-    }
-    bb.clear();
-  }
-   /**
-     *  Metode zum Verbinden
-     *  @since 0.0.1
-     */
-  private void connect() {
-    try {
-      c1 = DatagramChannel.open();
-      //c1.configureBlocking(false);
-      c1.socket().bind(null);
-    } catch (IOException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-    System.out.println("Connected");   
-  }
+	
+	
+	public Client(String addresse, int port, Spieler s) {
+		this.sself = s;
+		this.port = port;
+		bb = ByteBuffer.allocate(1024);
+		try {
+			InetAddress ip = InetAddress.getByName(addresse);
+			this.ip = ip;
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}
+		
+		SocketAddress address  = new InetSocketAddress(addresse, port);
+		this.address = address;
+		
+		try {
+			c1 = DatagramChannel.open();
+			c1.configureBlocking(false);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		hello();
+	} 
+
+	
+	
+	public void run() {
+		while(true) {
+			send();
+			read();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+	private void send(){
+		bb.clear();
+		bb.putInt(sself.getUUID());
+		bb.putDouble(sself.getZahl());
+		if(bb.position() != 0) {
+			//System.out.println("der sendbuffer enthält daten");
+		}
+		
+		bb.flip();
+		try {
+			c1.send(bb, address);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		bb.clear();	
+	}
+	
+	private void read() {
+		try {
+			c1.receive(bb);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if(bb.position() != 0) {
+			bb.flip();
+			System.out.println("first Int: "+bb.getInt());
+			System.out.println("first double: "+bb.getDouble());
+			//sself.setUUID(bb.getInt());
+			//sself.setZahl(bb.getDouble());
+		}
+		bb.clear();
+	}
+	
+	private void hello() {
+		System.out.println("Connect to Server...");
+		//Shakehand-Aktion. Es wird sich auf den Port geeinig, an den gesendet wird
+		String id = "Krokodeal"; //Der echte Username sollte mit get...() ausgelesen werden
+		byte[] buffer = id.getBytes();
+		byte[] get = new byte[1024];
+		
+		DatagramSocket toSocket = null;
+		DatagramSocket getSocket = null;
+		try {
+			toSocket = new DatagramSocket();
+			getSocket = new DatagramSocket(3555);
+			while(true) {
+				
+				
+				DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, ip, port);
+				toSocket.send(sendPacket);
+				DatagramPacket getPacket = new DatagramPacket(get,get.length);
+				getSocket.receive(getPacket);
+				
+				if(getPacket.getData().length != 0) {
+					port = getPacket.getPort();
+					ip = getPacket.getAddress();
+					System.out.println("Connected");
+					toSocket.close();
+					getSocket.close();
+					return;
+				}
+				
+			}
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
 }
