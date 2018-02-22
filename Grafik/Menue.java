111e97186c43f6f49cf1ff48770a4f38e6c0da3b
Taldarion <fasch00@icloud.com>
2018-02-22 18:58:57 +0100

    Update Menue.java
    Hat noch nen Bug beim zeichnen, sowohl Backgroud wird nicht gefärbt als auch die Buttons werden zweimal gezeichnet einmal normal so wie geplant und einmal klein oben.

--- Grafik/Menue.java
+++ Grafik/Menue.java
@@ -1,9 +1,11 @@
 package Grafik;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
@@ -11,14 +13,13 @@ public class Menue {
 	private MenueButton play;
 	//private MenueButton settings;
 	private MenueButton quit;
-	private String bgi;
+	private BufferedImage bgi = null;
 	private MouseListener pl = new MouseListener(){
 
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			// beginPlay()
-			System.out.println("Lass mich spielen!");
+			
 		}
 
 		@Override
@@ -36,13 +37,16 @@ public class Menue {
 		@Override
 		public void mousePressed(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			
+			play.changeClicked(true);
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			
+			play.changeClicked(false);
+			//LoginToServer()
+			//ChangeState(Heldenauswahl oder Bossauswahl)
+			System.out.println("Lass mich spielen!");
 		}
 		
 	};
@@ -52,9 +56,7 @@ public class Menue {
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			// quit()
-			System.out.println("Geh weg!");
-			System.exit(0);
+			
 		}
 
 		@Override
@@ -72,13 +74,15 @@ public class Menue {
 		@Override
 		public void mousePressed(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			
+			quit.changeClicked(true);
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			
+			quit.changeClicked(false);
+			System.out.println("Geh weg!");
+			System.exit(0);
 		}
 		
 	};
@@ -87,8 +91,9 @@ public class Menue {
 	
 	public Menue(){
 		p = new MenuePanel();
-		play = new MenueButton("Play", pl, 80, 80, 80, 30);
-		quit = new MenueButton("Quit", ql, 80, 120, 80, 30);
+		play = new MenueButton("Play", pl, 180, 80, 200, 80);
+		quit = new MenueButton("Quit", ql, 180, 200, 200, 80);
+		//p.setLayout(new BorderLayout());
 		p.add(play.getPanel());
 		p.add(quit.getPanel());
 	}
@@ -111,8 +116,9 @@ public class Menue {
 	  	@Override
 	  	public void paint(Graphics g) {
 	  		this.setBounds(0, 0, RenderManager.getFWidth(), RenderManager.getFHeight());
-	  		this.setBackground(Color.cyan);
 	  		this.paintComponents(g);
+	  		this.setBackground(Color.green);
 	  	}
 	  }
 }
+
