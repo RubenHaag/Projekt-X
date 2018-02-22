b7667802f944a496926832d48c5dbb3c28f0559f
OskarHowe <35808038+OskarHowe@users.noreply.github.com>
2018-02-22 17:03:02 +0100

    Client Version 0.2.1
    ShakeHands(), genannt hell0(), ist implementiert (der Client bekommt vom Server einen Port zugewisen, an den der Client ab jetzt die Packets versenden soll)
    Ich habe ohne java.NIO gearbeitet, da es sich beim gegenseitigen austausch der IPaddressen als komplzierter herausgestellt hat. Beim neu verwendetetn DatagramPacket kann man direkt auslesen, von welcher IP und welchem Port gesendet wurde.
    Allen Java.NIO code habe ich entfernt

--- Client-Server-Kommunikation/Client-Server (Version 0.1)/Client.java
+++ Client-Server-Kommunikation/Client-Server (Version 0.1)/Client.java
@@ -5,37 +5,37 @@ import java.nio.channels.DatagramChannel;
 
 /*
  * @author Oskar Moritz
- * @version 0.1
+ * @version 0.2.1
  */
 
 public class Client extends Thread{
 	
 	private int port;
-	private DatagramChannel c1;
-	private SocketAddress address;
-	private ByteBuffer bb;
 	private Spieler sself; //bekommen wir von Lukas im z.b. Konstruktor
+	private InetAddress ip;
 
 	
 	
 	public Client(String addresse, int port, Spieler s) {
 		this.sself = s;
 		this.port = port;
-		bb = ByteBuffer.allocate(1024);
-		SocketAddress address  = new InetSocketAddress(addresse, port);
-		this.address = address;
-		connect();
+		try {
+			InetAddress ip = InetAddress.getByName(addresse);
+			this.ip = ip;
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}
+		hello();
 	} 
 
 	
 	
 	public void run() {
 		while(true) {
-			System.out.println("Going to send soon");
 			send();
 			read();
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -43,53 +43,49 @@ public class Client extends Thread{
 		}
 	}
 	private void send(){
-		bb.clear();
-		bb.putInt(sself.getUUID());
-		bb.putDouble(sself.getZahl());
-		if(bb.position() != 0) {
-			System.out.println("der sendbuffer enthält daten");
-		}
-		
-		bb.flip();
-		try {
-			c1.send(bb, address);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		bb.clear();	
+
 	}
 	
 	private void read() {
+		
+	}
+	
+	private void hello() {
+		System.out.println("Connect to Server...");
+		//Shakehand-Aktion. Es wird sich auf den Port geeinig, an den gesendet wird
+		String id = "Krokodeal"; //Der echte Username sollte mit get...() ausgelesen werden
+		byte[] buffer = id.getBytes();
+		byte[] get = new byte[1024];
+		
+		DatagramSocket toSocket = null;
+		DatagramSocket getSocket = null;
 		try {
-			c1.receive(bb);
-			c1.is
+			toSocket = new DatagramSocket();
+			getSocket = new DatagramSocket(3555);
+			while(true) {
+				
+				
+				DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, ip, port);
+				toSocket.send(sendPacket);
+				DatagramPacket getPacket = new DatagramPacket(get,get.length);
+				getSocket.receive(getPacket);
+				
+				if(getPacket.getData().length != 0) {
+					port = getPacket.getPort();
+					ip = getPacket.getAddress();
+					System.out.println("Connected");
+					toSocket.close();
+					getSocket.close();
+					return;
+				}
+				
+			}
+		
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println("kurz vor if abfrage");
-		if(bb.position() != 0) {
-			bb.flip();
-			System.out.println("first Int: "+bb.getInt());
-			System.out.println("first double: "+bb.getDouble());
-			//sself.setUUID(bb.getInt());
-			//sself.setZahl(bb.getDouble());
-		}
-		bb.clear();
-	}
-	
-	private void connect() {
-		try {
-			c1 = DatagramChannel.open();
-			c1.configureBlocking(false);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		System.out.println("Connected");
-
 		
-
 		
 	}
 
