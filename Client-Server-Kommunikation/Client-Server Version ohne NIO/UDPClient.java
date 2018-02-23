d4f14f0a37334a9e8a76db0160b6b94df291f613
Unknown <moritz_gerlach@mac.com>
2018-02-24 00:12:46 +0100

    Verbesserung
    Verbesserung der Lesbarkeit und Verbesserung JavaDoc

--- Client-Server-Kommunikation/Client-Server Version ohne NIO/UDPClient.java
+++ Client-Server-Kommunikation/Client-Server Version ohne NIO/UDPClient.java
@@ -2,7 +2,11 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-
+/**
+ * UDPClient ist der Client. UDPClient dient zum Daten emfangen, von dem Server und zum senden, an den Server. Ohne java.nio. Nur mit java.net. UDP-Verbindung: "verbindungslos" Verbindung 
+ * @author  Oskar, Moritz
+ * @version 0.3
+ */
 public class UDPClient extends Thread {
   private DatagramSocket datagramSocket, datagramSocketSend;
   private final int port = 1409;
@@ -10,9 +14,9 @@ public class UDPClient extends Thread {
   private InetAddress ia1;
   
   /**
-     * Konstruktor für die Klasse UDPServer
+     * Konstruktor für die Klasse UDPClient
      * @param ia ist die InetAddress von dem Server, an den der Client die Daten sendet.
-     * @param port ist der port an dem der Client die Daten sendet.
+     * @param port ist der port an dem der Client die Daten sendet soll.
      */
   public UDPClient(InetAddress ia, int port) throws IOException {
     ia1 = ia;
@@ -21,47 +25,47 @@ public class UDPClient extends Thread {
   }
   
   public void run() {
-      while ( true ){
-        try {
+    while ( true ){
+      try {
         send();
         read();
       } catch (IOException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
       }
-        
-        try {
-           Thread.sleep(100);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
+      
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
   }
-   /*  Metode zum eröffen des Clients
+   /*  Diese Methode eröffnet den Client. Diese Methode erstellt zwei „datagramSocket“. Der erste (datagramSocket) wird genutzt um daten zu empfangen @see read() . Der andern (datagramSocketSend) wird zum Senden benötigt. @see send() 
      *  @since 0.0.1
      */
   private void connect() throws IOException {
-      datagramSocket = new DatagramSocket(port);
-      datagramSocketSend =new DatagramSocket();
-    } 
+    datagramSocket = new DatagramSocket(port);
+    datagramSocketSend =new DatagramSocket();
+  } 
   
    /**
-     *  Metode zum senden von den Daten.
+     *  Die send()-Methode ist zum Senden, der Daten notwendig. Mit dieser Methode werden Daten an den Server gesendet. Dies passiert über den datagramSocketSend und das „DatagramPacket“.
      */
-    private void send() throws IOException {
+  private void send() throws IOException {
     byte[] sandData = "Hallo".getBytes();
     DatagramPacket packet = new DatagramPacket( sandData, sandData.length, ia1, portToServer );
     datagramSocketSend.send(packet);
-    }
+  }
     /**
-     *  Metode zum empfangen von den Daten.
+     *  Die Methode read() ist zum empfangen, der Daten notwendig. In dieser Methode wird ein „DatagramPacket“ erstellt. Dann wird gewartet bis das „DatagramPacket“ angekommen ist und kann das „DatagramPacket“ ausgelesen werden.
      */
-    private void read() throws IOException {
-      DatagramPacket packet= new DatagramPacket( new byte[1024], 1024 );
-      datagramSocket.receive( packet );
+  private void read() throws IOException {
+    DatagramPacket packet= new DatagramPacket( new byte[1024], 1024 );
+    datagramSocket.receive( packet );
     byte[] data = packet.getData();
     System.out.println("String: "+data.toString());
-    }
+  }
 
 }
 
--- Client-Server-Kommunikation/Client-Server Version ohne NIO/UDPServer.java
+++ Client-Server-Kommunikation/Client-Server Version ohne NIO/UDPServer.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.net.*;
 
 /**
- * UDPServer dient zum Daten emfangen, von den Clients und zum senden, an die Clients. Ohne Nio. UDPServer
+ * UDPServer dient zum Daten emfangen, von den Clients und zum senden, an die Clients. Ohne java.nio. Nur mit java.net. UDP-Verbindung: "verbindungslos" Verbindung 
  * @author  Oskar, Moritz
  * @version 0.3
  */
@@ -17,7 +17,7 @@ public class UDPServer extends Thread{
   private InetAddress ia1, ia2, ia3;
     
   /**
-     * Konstruktor für die Klasse UDPServer
+     * Konstruktor für die Klasse UDPServer. Der Konstruktor führt folgen aufgaben aus: Erstens – Hier werden die Übergeben Werte in „Klassen-Variabel“ gespeichert so, dass später noch auf diese Werte zugreifen werden kann. Zweites – Hier wird die „private-Methode“ connect() ausgeführt. @see connect() 
      * @param ia1 ist die InetAddress von dem ersten Client, an den der Server daten sendet.
      * @param ia2 ist die InetAddress von dem zweiten Client, an den der Server daten sendet.
      * @param ia3 ist die InetAddress von dem dritten Client, an den der Server daten sendet.
@@ -30,22 +30,22 @@ public class UDPServer extends Thread{
   }
     
   public void run() {
-      while ( true ){
-        try {
-          read1();
-          //read2();
-          //read3();
+    while ( true ){
+      try {
+        read1();
+        read2();
+        read3();
         send();
       } catch (IOException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
       }
-        
-        try {
-           Thread.sleep(100);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
+      
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
   }
     
@@ -54,50 +54,49 @@ public class UDPServer extends Thread{
      */
   private void read1() throws IOException {
     packet= new DatagramPacket( new byte[1024], 1024 );
-      datagramSocket1.receive( packet );
+    datagramSocket1.receive( packet );
     byte[] data = packet.getData();
     System.out.println("String 1: "+data.toString());
-    }
+  }
   /**
      *  Metoden zum emfangen von Daten und zum verarbeiten der Daten. von einem Client. (Client 2)
      */
   private void read2() throws IOException {
     packet= new DatagramPacket( new byte[1024], 1024 );
-      datagramSocket2.receive( packet );
+    datagramSocket2.receive( packet );
     byte[] data = packet.getData();
     System.out.println("String 2: "+data.toString());
-    }
+  }
   /**
      *  Metoden zum emfangen von Daten und zum verarbeiten der Daten. von einem Client. (Client 3)
      */
   private void read3() throws IOException {
     packet= new DatagramPacket( new byte[1024], 1024 );
-      datagramSocket3.receive( packet );
+    datagramSocket3.receive( packet );
     byte[] data = packet.getData();
     System.out.println("String 3: "+data.toString());
-    }
+  }
     /**
      *  Metode zum senden von den Daten.
      */
-    private void send() throws IOException {
+  private void send() throws IOException {
     byte[] sandData = "Hallo".getBytes();
-      packet = new DatagramPacket( sandData, sandData.length, ia1, portToC );
-      datagramSocketSend.send(packet);
-      packet = new DatagramPacket( sandData, sandData.length, ia2, portToC );
-      datagramSocketSend.send(packet);
-      packet = new DatagramPacket( sandData, sandData.length, ia3, portToC );
-    }
+    packet = new DatagramPacket( sandData, sandData.length, ia1, portToC );
+    datagramSocketSend.send(packet);
+    packet = new DatagramPacket( sandData, sandData.length, ia2, portToC );
+    datagramSocketSend.send(packet);
+    packet = new DatagramPacket( sandData, sandData.length, ia3, portToC );
+  }
     
-    /**
-     *  Metode zum eröffen des Servers
+    /*  Diese Methode eröffnet den Server. Diese Methode erstellt vier „datagramSocket“. Die ersten drei „datagramSockets“ wird genutzt um daten zu empfangen, die die Clients senden. Der andern (datagramSocketSend) wird zum Senden benötigt.  
      *  @since 0.0.1
      */
-    private void connect() throws IOException {
-      datagramSocket1 = new DatagramSocket( portRead1 );
-      datagramSocket2 = new DatagramSocket( portRead2 );
-      datagramSocket3 = new DatagramSocket( portRead3 );
-      datagramSocketSend =new DatagramSocket( );
-    } 
+  private void connect() throws IOException {
+    datagramSocket1 = new DatagramSocket( portRead1 );
+    datagramSocket2 = new DatagramSocket( portRead2 );
+    datagramSocket3 = new DatagramSocket( portRead3 );
+    datagramSocketSend =new DatagramSocket( );
+  } 
       
     
 }
