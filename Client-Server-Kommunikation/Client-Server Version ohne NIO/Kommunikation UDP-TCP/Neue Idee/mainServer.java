8dcdd20c382f0927a49afec51d97cf11644c4d12
Taldarion <fasch00@icloud.com>
2018-03-03 20:02:35 +0100

    abc

---
+++ Client-Server-Kommunikation/Client-Server Version ohne NIO/Kommunikation UDP-TCP/Neue Idee/mainServer.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class mainServer {
+
+  public static void main(String[] args) throws UnknownHostException, IOException {
+    Server l = new Server();
+
+  }
+
+}
