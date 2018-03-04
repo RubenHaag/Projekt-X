67771e7c9b78cb85f6bc7b0cb121269854c2c1c3
Taldarion <fasch00@icloud.com>
2018-03-04 11:22:01 +0100

    Animations Update + Map Implementierung
    Die Animationen werden nun flüssig dargestellt, aufgrund der Verschiebung in einen Thread.
    Die Map ist nun implementiert.

--- Grafik/src/Grafik/Player.java
+++ Grafik/src/Grafik/Player.java
@@ -25,15 +25,13 @@ class Player extends JComponent{
   private boolean right;
   private boolean takedmg;
   private boolean dead;
-  private short zaehler;
+  private short deadZaehler;
+  private short attackZaehler;
   private short zaehler2;
   private MovementType movement;
-  private Image[] image;
-  private short animationZaehler;
-  private short imageZaehler;
+  private PlayerAnimation anim;
   private BufferedImage idle;
   private BufferedImage dmg;
-  private BufferedImage[][] animations;
   private AttackType attack;
   
   /**
@@ -47,54 +45,26 @@ class Player extends JComponent{
    * @param r Blickrichtung des Spielers, true = rechts
  * @throws IOException 
    */
-  Player(String name, int x, int y, int w, int h, boolean r) throws IOException{
+  Player(String name, int x, int y, int w, int h, boolean r){
       this.name = name;
       this.x = x;
       this.y = y;
       width = w;
       height = h;
-      movement = MovementType.MOVE;
+      movement = MovementType.JUMPING;
       attack = AttackType.NON;
-      image = null;
       right = r;
-      zaehler = 0;
+      deadZaehler = 0;
       zaehler2 = 0;
-      image = new Image[6];
-      animations = new BufferedImage[6][5];
-      animationZaehler = 0;
-      imageZaehler = 0;
-      /*Toolkit toolkit = Toolkit.getDefaultToolkit();
-      image[0] = toolkit.createImage("Assets/Charaktere/" + name + "_move.gif");
-      image[1] = toolkit.createImage("Assets/Charaktere/" + name + "_jumping.gif");
-      image[2] = toolkit.createImage("Assets/Charaktere/" + name + "_attack.gif");
-      image[3] = toolkit.createImage("Assets/Charaktere/" + name + "_attack.gif");
-      image[4] = toolkit.createImage("Assets/Charaktere/" + name + "_attack.gif");
-      image[5] = toolkit.createImage("Assets/Charaktere/" + name + "_dead.gif");
-      for(Image i:image) {
-    	  i = i.getScaledInstance(width, height, 0);
-    	  i.setAccelerationPriority((float) 0.999);
-      }*/
-      
-      //@Ruben es gibt aus irgendeinem Grund jetzt eine FileNotFoundException obwohl die Dateinamen alle richtig sind
-      for(int i = 1; i < 6; i++) {
-    	 animations[0][i-1] = ImageIO.read(new File("Assets/Charaktere/" + name + "/" + name + "_laufen(" + i + ").png"));
-      }
-      for(int i = 0; i <= 5; i++) {
-     	 animations[1][i-1] = ImageIO.read(new File("Assets/Charaktere/" + name + "/" + name + "_jump (" + i + ").png"));
-       }
-      for(int i = 0; i <= 5; i++) {
-     	 animations[2][i-1] = ImageIO.read(new File("Assets/Charaktere/" + name + "/" + name + "_attack(" + i + ").png"));
-       }
-      for(int i = 0; i <= 5; i++) {
-     	 animations[3][i-1] = ImageIO.read(new File("Assets/Charaktere/" + name + "/" + name + "_attack(" + i + ").png"));
-       }
-      for(int i = 0; i <= 5; i++) {
-     	 animations[4][i-1] = ImageIO.read(new File("Assets/Charaktere/" + name + "/" + name + "_attack(" + i + ").png"));
-       }
-      for(int i = 0; i <= 5; i++) {
-     	 animations[5][i-1] = ImageIO.read(new File("Assets/Charaktere/" + name + "/" + name + "_dead (" + i + ").png"));
-       }
+      attackZaehler = 0;
+      anim = new PlayerAnimation(name);
     }
+  
+  public void initPlayerAnimations() throws IOException {
+	  anim.initPlayerAnimations();
+	  anim.start();
+	  anim.setAnimation(2);
+  }
   
   /**
    * Aktualisiert die Position des Spielers
@@ -114,29 +84,19 @@ class Player extends JComponent{
   public void updateMovementType(MovementType mt, boolean r){
     movement = mt;
     right = r;
-    System.out.println(r);
     switch(movement){
     case IDLE:
-  	  if(idle == null){
-            try {
-              idle = ImageIO.read(new File("Assets/Charaktere/" + name + "_idle.png"));
-            } catch (IOException e) {
-              // TODO Auto-generated catch block
-              e.printStackTrace();
-            }
-          }
   	  break;
     case MOVE:
-    	animationZaehler = 0;
+    	anim.setAnimation(0);
     	break;
     case JUMPING:
-    	animationZaehler = 1;
+    	anim.setAnimation(1);
     	break;
     default:
     	System.out.println("Irgendwas ist schiefgelaufen");
     	break;
     }
-    imageZaehler = 0;
   }
   
   /**
@@ -151,18 +111,20 @@ class Player extends JComponent{
       break;
     case 1:
       attack = AttackType.NORMAL;
-      animationZaehler = 2;
+      anim.setAnimation(2);
+      attackZaehler = 0;
       break;
     case 2:
       attack = AttackType.SPECIAL1;
-      animationZaehler = 3;
+      anim.setAnimation(3);
+      attackZaehler = 0;
       break;
     case 3:
       attack = AttackType.SPECIAL2;
-      animationZaehler = 4;
+      anim.setAnimation(4);
+      attackZaehler = 0;
       break;
     }
-    imageZaehler = 0;
   }
   
   /**
@@ -177,7 +139,9 @@ class Player extends JComponent{
    */
   public void die() {
     dead = true;
+    anim.setAnimation(5);
   }
+  
   public void repaint() {
 	  super.repaint(0, x, y, width, height);
 	  
@@ -188,73 +152,76 @@ class Player extends JComponent{
    * Zeichnet den Spieler je nach Bewegungsart, Angriffstyp und ob er gerade Schaden erlitten hat oder gestorben ist
    */
   public void paintComponent(Graphics g) {
-      //Graphics2D g2d = (Graphics2D) g;
-      this.setBounds(x, y, width, height);
-      // erst Bewegungsanamation laden
-      
-      //dann Angriff
-//      switch(attack){
-//      case NON:
-//        //falls kein Angriff ausgeführt wird, normal die Bewegung zeichnen
-//        break;
-//      case NORMAL:
-//    	imageZaehler = 2;
-//    	  break;
-//      case SPECIAL1:
-//    	imageZaehler = 3;
-//        break;
-//      case SPECIAL2:
-//    	imageZaehler = 4;
-//        break;
-//      }
-//      if(dead) {
-//    	  imageZaehler = 5;
-//    	  zaehler++;
-//      }
-      
-      if(takedmg) {
-    	  if(dmg == null){
-              try {
-                dmg = ImageIO.read(new File("Assets/Charaktere/" + name + "_damage.png"));
-              } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-              }
-          }
-      }
-      if(right && movement != MovementType.IDLE && !takedmg) {
-    	  g.drawImage(animations[animationZaehler][imageZaehler], 0, 0, width, height, this);
-    	  imageZaehler++;
-      }
-      else if(!right && movement != MovementType.IDLE && !takedmg){
-    	  g.drawImage(animations[animationZaehler][imageZaehler], width-1, 0, -width, height, this);
-    	  imageZaehler++;
-      }
-      else if(right && movement == MovementType.IDLE && !takedmg) {
-    	  g.drawImage(idle, 0, 0, width, height, null);
-      }
-      else if(!right && movement == MovementType.IDLE && !takedmg) {
-    	  g.drawImage(idle, width-1, 0, -width, height, null);
-      }
-      else if(right && takedmg) {
-    	  g.drawImage(dmg, 0, 0, width, height, null);
-    	  zaehler2++;
-      }
-      else {
-    	  g.drawImage(dmg, width-1, 0, -width, height, null);
-    	  zaehler2++;
-      }
-      if(zaehler >= 5) {
-    	  Game.delPlayer(this);
-      }
-      if(zaehler2 >= 5) {
-    	  takedmg = false;
-    	  zaehler2 = 0;
-      }
-      if(imageZaehler >= 5) {
-    	  imageZaehler = 0;
-      }
-      
+	  this.setBounds(x, y, width, height); 
+	  if(!dead) {
+		  if(takedmg) {
+		    	if(dmg == null){
+		    		try {
+		    			dmg = ImageIO.read(new File("Assets/Charaktere/" + name + "_damage.png"));
+		    		} catch (IOException e) {
+		    			// TODO Auto-generated catch block
+		    			e.printStackTrace();
+		    		}
+		    	}
+		    	if(right) {
+		        	g.drawImage(dmg, 0, 0, width, height, null);
+		        	zaehler2++;
+		        }
+		        else {
+		        	g.drawImage(dmg, width-1, 0, -width, height, null);
+		        	zaehler2++;
+		        }
+		  }
+		  else {
+			  if(movement == MovementType.IDLE) {
+				  if(idle == null){
+			            try {
+			              idle = ImageIO.read(new File("Assets/Charaktere/" + name + "_idle.png"));
+			            } catch (IOException e) {
+			              // TODO Auto-generated catch block
+			              e.printStackTrace();
+			            }
+			      }
+				  if(right) {
+		        	g.drawImage(idle, 0, 0, width, height, null);
+				  }
+				  else {
+		        	g.drawImage(idle, width-1, 0, -width, height, null);
+				  }
+			  }
+			  else {
+				  if(attack != AttackType.NON) {
+				      attackZaehler++;
+				  }
+		    	if(right) {
+		        	g.drawImage(anim.getImage(), 0, 0, width, height, this);
+		    	}
+		        else {
+		        	g.drawImage(anim.getImage(), width-1, 0, -width, height, this);
+		        }
+			  }  		  
+		   }  
+	  }
+	  else {
+		if(right) {
+      		g.drawImage(anim.getImage(), 0, 0, width, height, this);
+  		}
+      	else {
+      		g.drawImage(anim.getImage(), width-1, 0, -width, height, this);
+      	}
+		deadZaehler++;
+	  }
+	  if(deadZaehler >= 5) {
+		  Game.delPlayer(this);
+	  }
+	  if(zaehler2 >= 5) {
+		  takedmg = false;
+		  zaehler2 = 0;
+	  }
+	  if(attackZaehler >= 5) {
+		  attack = AttackType.NON;
+		  attackZaehler = 0;
+	  }
   }
   
   /**
